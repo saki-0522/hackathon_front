7d6c91d99f85f9417a9002d056124f06446b27f1
saki-0522 <sakinokimochi@gmail.com>
2024-06-13 11:59:54 +0900

    test

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './logo/logo.svg';
+// import logo from './logo/logo.svg';
 import './App.css';
 import LoginForm from "./pages/Login"
 import { onAuthStateChanged } from 'firebase/auth';
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
